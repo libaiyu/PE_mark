7be0f581415233f1284b532c260ec74a0bc7008f
Libaiyu <709097185@qq.com>
2017-07-13 00:39:28 +0800

    Change some file names in readme.txt .

--- PE_mark_make.py
+++ PE_mark_make.py
@@ -1,8 +1,9 @@
 
 
-''' make the score dict of the file(“ 高考体育成绩对照表”), from the file
-(“高考体育成绩测量表”)to get the information, then look it up in the score dict
-to get the score. Finally write the score to a new file.     2017-7-12 '''
+''' make the score dict of the file(《标准表甲》《标准表乙》
+《标准表丙》《标准表丁》）, from the file(《高考体育成绩测量表》)
+to get the information,then look it up in the score dict to get
+the score. Finally write the score to a new file.     2017-7-12 '''
 
 
 import os
--- readme.txt
+++ readme.txt
@@ -1,4 +1,3 @@
 
-make the score dict of the file(“ 高考体育成绩对照表”), from the file
-(“高考体育成绩测量表”)to get the information, then look it up in the score dict
+make the score dict of the file(《标准表甲》《标准表乙》《标准表丙》《标准表丁》）, from the file(《高考体育成绩测量表》)to get the information, then look it up in the score dict
 to get the score. Finally write the score to a new file.     2017-7-12
