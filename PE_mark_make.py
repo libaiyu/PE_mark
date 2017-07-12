de9b694a0d0726ddfa5696439473b3d47f1b2186
Libaiyu <709097185@qq.com>
2017-07-13 00:30:05 +0800

    mark change to score.

--- PE_mark_make.py
+++ PE_mark_make.py
@@ -1,8 +1,8 @@
 
 
-''' make the mark list of the file(“ 高考体育成绩对照表”), from the file
-(“高考体育成绩测量表”)to get the information, then look it up in the mark dict
-to get the mark. Finally write the mark to a new file.     2017-7-12 '''
+''' make the score dict of the file(“ 高考体育成绩对照表”), from the file
+(“高考体育成绩测量表”)to get the information, then look it up in the score dict
+to get the score. Finally write the score to a new file.     2017-7-12 '''
 
 
 import os
---
+++ readme.txt
@@ -0,0 +1,4 @@
+
+make the score dict of the file(“ 高考体育成绩对照表”), from the file
+(“高考体育成绩测量表”)to get the information, then look it up in the score dict
+to get the score. Finally write the score to a new file.     2017-7-12
